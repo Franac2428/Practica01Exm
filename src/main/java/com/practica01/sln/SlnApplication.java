b6850ad4d4982b4559e806998083d805057f3428
Fran <HP PROBOOK@DESKTOP-IGJKM84>
2023-02-27 20:58:55 -0600

    Práctica Examen V2

---
+++ src/main/java/com/practica01/sln/SlnApplication.java
@@ -0,0 +1,13 @@
+package com.practica01.sln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica01/sln/SlnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01.sln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
